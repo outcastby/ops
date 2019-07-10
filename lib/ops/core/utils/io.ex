483e7f64a2f5d304d0ab3f02473aa2ba1e54cb88
denislutski <avenger_den@mail.ru>
2019-07-10 17:45:37 +0300

    Fix bad method name

--- lib/ops/core/utils/io.ex
+++ lib/ops/core/utils/io.ex
@@ -1,5 +1,5 @@
 defmodule Ops.Utils.Io do
   def settings(text, color), do: [:black_background, color, text] |> IO.ANSI.format()
-  def puts(text, color \\ :green), do: text |> get_options(color) |> IO.puts()
-  def write(text, color \\ :green), do: text |> get_options(color) |> IO.write()
+  def puts(text, color \\ :green), do: text |> settings(color) |> IO.puts()
+  def write(text, color \\ :green), do: text |> settings(color) |> IO.write()
 end
